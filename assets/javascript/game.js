b3a72cf85c5f32a5159ae88b74bff424f3f53424
Tyler Shuck <tshuck88@gmail.com>
2019-09-10 17:59:47 -0700

    created if/else statements for correct and incorrect user guesses. created a game state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 var wins = 0;
-var guessesRemaining = 12;
-var lettersGuessed = [];
+var guessesRemaining;
+var lettersGuessed;
+var currentWord;
+var currentWordBlank;
 var possibleWords = ["Cardinals", "Falcons", "Ravens", "Bills", "Panthers", "Bears", "Bengals", "Browns", "Cowboys", "Broncos", "Lions", "Packers", "Texans", "Colts", "Jaguars", "Chiefs", "Chargers", "Rams", "Dolphins", "Vikings", "Patriots", "Saints", "Giants", "Jets", "Raiders", "Eagles", "Steelers", "49ers", "Seahawks", "Buccaneers", "Titans", "Redskins"];
 
 var winsText = document.getElementById("wins-text");
@@ -8,28 +10,59 @@ var currentWordText = document.getElementById("current-word-text");
 var guessesRemainingText = document.getElementById("guesses-remaining-text");
 var lettersGuessedText = document.getElementById("letters-guessed-text");
 
-var currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-console.log(currentWord)
-
-var currentWordBlank = [];
-for (var i = 0; i < currentWord.length; i++){
-    currentWordBlank[i] = "_ ";
-}
-
-// guessesRemainingText.textContent = guessesRemaining;
-// currentWordText.textContent = currentWordBlank
-
-console.log(currentWordBlank)
 
 
+initializeGameState();
 
-document.onkeyup = function(event) {
-    var userGuess = event.key.toLowerCase;
-    
-    for(i = 0; i < currentWord.length; i++){
-        if(userGuess === currentWord.charAt(i)){
-            currentWordBlank
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var correctLetter = false;
+
+    if(!lettersGuessed.includes(userGuess)){
+        for (var j = 0; j < currentWord.length; j++) {
+            if (userGuess === currentWord[j].toLowerCase()) {
+                currentWordBlank[j] = userGuess;
+                correctLetter = true;
+            }
         }
+        if (correctLetter) {
+            if(!currentWordBlank.includes("_")){
+                wins++;
+                initializeGameState();
+            }
+        }
+        else {
+            lettersGuessed.push(userGuess);
+            guessesRemaining--;  
+            if(guessesRemaining === 0){
+                initializeGameState();
+            }
+        }
+        console.log(currentWordBlank)
+        displayGameState();
     }
+    // console.log(userGuess)
+    // console.log(lettersGuessed)
+    // console.log(guessesRemaining)
+
 }
 
+
+function displayGameState(){
+    currentWordText.textContent = currentWordBlank.join(" ");
+    guessesRemainingText.textContent = guessesRemaining;
+    winsText.textContent = wins;
+    lettersGuessedText.textContent = lettersGuessed.join(", ")
+}
+
+function initializeGameState() {
+    currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+    console.log(currentWord);
+    guessesRemaining = 12;
+    lettersGuessed = [];
+    currentWordBlank = [];
+    for (var i = 0; i < currentWord.length; i++) {
+        currentWordBlank[i] = "_";
+    }
+    displayGameState();
+}
