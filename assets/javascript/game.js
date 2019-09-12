cd94dd742cad4a781a970d15ffc7535b73d6823b
Tyler Shuck <tshuck88@gmail.com>
2019-09-12 11:28:41 -0700

    added comments to game.js, moved the displayImage function to call before initializeGameState function so the image is of the winning word and not the new word. added winning and losing word text to the displayImage function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,51 @@
-var wins = 0;
-var guessesRemaining;
-var lettersGuessed;
-var currentWord;
-var currentWordBlank;
+var wins = 0; // variable to count wins
+var guessesRemaining; // variable to count guesses remaining
+var lettersGuessed; // variable to store letters guessed
+var currentWord; // variable to store current word
+var currentWordBlank; // variable to store the current word as underscores
+
+// array of the possible words
 var possibleWords = ["Cardinals", "Falcons", "Ravens", "Bills", "Panthers", "Bears", "Bengals", "Browns", "Cowboys", "Broncos", "Lions", "Packers", "Texans", "Colts", "Jaguars", "Chiefs", "Chargers", "Rams", "Dolphins", "Vikings", "Patriots", "Saints", "Giants", "Jets", "Raiders", "Eagles", "Steelers", "49ers", "Seahawks", "Buccaneers", "Titans", "Redskins"];
 
+// variables to get a handle on the corresponding html elements
 var winsText = document.getElementById("wins-text");
 var currentWordText = document.getElementById("current-word-text");
 var guessesRemainingText = document.getElementById("guesses-remaining-text");
 var lettersGuessedText = document.getElementById("letters-guessed-text");
-// var imageContentDisplay = document.getElementById("imgage-content")
+var winningWordText = document.getElementById("winning-word-text");
+var imageContentDisplay = document.getElementById("img-content");
 
+initializeGameState(); // call the function to start the game on page load
 
-initializeGameState();
-
-document.onkeyup = function (event) {
-    var userGuess = event.key.toLowerCase();
-    var correctLetter = false;
+document.onkeyup = function (event) { // function to listen to key events to play the game
+    var userGuess = event.key.toLowerCase(); // variable to store user guesses as lower case letters
+    var correctLetter = false; // set the correct letter to default as false
 
     if (userGuess.match(/^[a-z0-9]+$/i)) {
-        if (!lettersGuessed.includes(userGuess)) {
-            for (var j = 0; j < currentWord.length; j++) {
-                if (userGuess === currentWord[j].toLowerCase()) {
-                    currentWordBlank[j] = userGuess;
-                    correctLetter = true;
+        if (!lettersGuessed.includes(userGuess)) { // if letters already guessed do not include the user guess
+            for (var j = 0; j < currentWord.length; j++) { // then run a for loop the length of the current word
+                if (userGuess === currentWord[j].toLowerCase()) { // if the user guess equals a letter in the current word
+                    currentWordBlank[j] = userGuess; // then replace the correspending current word blank letter with the user guess
+                    correctLetter = true; // set the correct letter to true
                 }
             }
-            if (correctLetter) {
-                if (!currentWordBlank.includes("_")) {
-                    wins++;
-                    initializeGameState();
-                    displayImage();
+            if (correctLetter) { // if correct letter is true
+                if (!currentWordBlank.includes("_")) { // and if the current word blank doesn't include any underscores
+                    wins++; // then the user has won and so increase the win count by 1
+                    displayImage(); // call the function to display the correspending team image and winning text
+                    initializeGameState(); // call the function to reset the game 
                 }
             }
             else {
-                lettersGuessed.push(userGuess);
-                guessesRemaining--;
-                if (guessesRemaining === 0) {
-                    initializeGameState();
+                lettersGuessed.push(userGuess); // if the user guess is not correct then push that to the lettersGuessed array
+                guessesRemaining--; // decrease the guesses remaining by 1
+                if (guessesRemaining === 0) { // if the guess remaining equals 0 the user has lost
+                    displayImage(); // display the corresponding losing image and text
+                    initializeGameState(); // reset the game
                 }
             }
             console.log(currentWordBlank)
-            displayGameState();
+            displayGameState(); // call the function to update the game text after the user has pressed a key that hasn't been guessed yet
         }
     }
     // else {
@@ -49,31 +53,39 @@ document.onkeyup = function (event) {
 
 
     // }
-}
+};
 
-
+// function to update game text with the current game state
 function displayGameState() {
-    currentWordText.textContent = currentWordBlank.join(" ");
-    guessesRemainingText.textContent = guessesRemaining;
+    currentWordText.textContent = currentWordBlank.join(" "); // puts a space in between each letter of the current word blank
+    guessesRemainingText.textContent = guessesRemaining; 
     winsText.textContent = wins;
-    lettersGuessedText.textContent = lettersGuessed.join(", ")
-}
+    lettersGuessedText.textContent = lettersGuessed.join(", ") // puts a comma and space in between each letter the user has guessed
+};
 
+// function to start on page load and reset the game when the user wins or loses
 function initializeGameState() {
-    currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+    currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)]; // randomly generates a word in the possibleWords array
     console.log(currentWord);
-    guessesRemaining = 12;
-    lettersGuessed = [];
-    currentWordBlank = [];
-    for (var i = 0; i < currentWord.length; i++) {
+    guessesRemaining = 12; // sets the initial guesses to 12
+    lettersGuessed = []; // empty array to store letters guessed
+    currentWordBlank = []; // empty array to store the current word letters
+    for (var i = 0; i < currentWord.length; i++) { // for loop that converts each letter in the currentWordBlank array to underscores
         currentWordBlank[i] = "_";
     }
-    displayGameState();
-}
+    displayGameState(); // calls the function to update the game text each time the initializeGameState function is called
+};
 
+// function to display an image depending on if the user wins or loses and what the current word is and what the winning word is
 function displayImage() {
-   var imageContentDisplay = document.getElementById("img-content");
-   imageContentDisplay.setAttribute("src", "../word-guess-game/assets/images/" + currentWord.toLowerCase() + ".png");
-   imageContentDisplay.setAttribute("width", "300px");
-   imageContentDisplay.setAttribute("height", "auto");   
-}
+    imageContentDisplay.setAttribute("width", "300px");
+    imageContentDisplay.setAttribute("height", "auto");
+
+    if (guessesRemaining === 0) {
+        imageContentDisplay.setAttribute("src", "../word-guess-game/assets/images/you-lose.png");
+        winningWordText.textContent = "You lost. The word was " + currentWord + ".";
+    } else {
+        imageContentDisplay.setAttribute("src", "../word-guess-game/assets/images/" + currentWord.toLowerCase() + ".png");
+        winningWordText.textContent = "You won! The word was " + currentWord + "!";
+    }
+};
